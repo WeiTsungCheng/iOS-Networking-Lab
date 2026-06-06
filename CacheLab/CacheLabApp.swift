bb0d37781611e06f08cf8ad6eef6965e3ba28468
WEI-TSUNG CHENG <3118197C@student.gla.ac.uk>
2026-06-06 15:19:26 +0100

    Initial Commit

---
+++ CacheLab/CacheLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheLabApp.swift
+//  CacheLab
+//
+//  Created by WEITSUNG on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CacheLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
